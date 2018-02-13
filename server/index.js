fb23fd2c803734ab3813bc100d983955ff905b6c
unknown <jordanmbush@gmail.com>
2018-02-13 14:10:36 -0700

    completed Step 2

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const app = express();
 require('dotenv').config();
+
+app.use(bodyParser.json());
+
+app.use( session({
+    secret: process.env.SECRET_KEY,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        age: 6000
+    }
+}))
+
+const PORT = 4000;
+app.listen( PORT, () => console.log(`Hey Jordan. I\'m over here on port ${PORT}!`));
